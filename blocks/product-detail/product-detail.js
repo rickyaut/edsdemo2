f49ed6ac579d726788acdf1894c502f98e00c878
Ricky Liu <ricky.liu@merkleinc.com.au>
2024-07-30 19:56:11 +1000

    add product-detail

--- blocks/product-detail/product-detail.js
+++ blocks/product-detail/product-detail.js
@@ -2,6 +2,6 @@ export default async function decorate(block) {
     const resp = await fetch(`/products.json`);
     const json = await resp.json();
     json.data.forEach((product) => {
-      console.log(prouct.Name);
+      console.log(product.Name);
     });
 }
