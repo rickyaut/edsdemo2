a5b7dbff65dbd80f15c79ae2f8cf6b1e06433a0c
Ricky Liu <ricky.liu@merkleinc.com.au>
2024-07-30 20:48:00 +1000

    add product-list

---
+++ blocks/product-list/product-list.js
@@ -0,0 +1,16 @@
+export default async function decorate(block) {
+    const productsURL = block.querySelector('a[href$=".json"]');
+    if (!productsURL) return;
+      const resp = await fetch(productsURL);
+    const json = await resp.json();
+    block.innerHTML = ''
+    json.data.forEach((product) => {
+      productArea = document.createElement('div')
+      block.append(productArea)
+      productArea.setAttribute("class", "product")
+      productName = document.createElement('div')
+      productName.setAttribute('class', 'product-name')
+      productName.innerHTML = product.Name
+      productArea.append(productName)
+    });
+}
