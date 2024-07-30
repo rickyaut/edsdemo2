cab8396598f70d13f6352a305ae998d579fa2b0d
Ricky Liu <ricky.liu@merkleinc.com.au>
2024-07-30 20:55:47 +1000

    add product-list

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -5,10 +5,10 @@ export default async function decorate(block) {
     const json = await resp.json();
     block.innerHTML = ''
     json.data.forEach((product) => {
-      productArea = document.createElement('div')
+      var productArea = document.createElement('div')
       block.append(productArea)
       productArea.setAttribute("class", "product")
-      productName = document.createElement('div')
+      var productName = document.createElement('div')
       productName.setAttribute('class', 'product-name')
       productName.innerHTML = product.Name
       productArea.append(productName)
